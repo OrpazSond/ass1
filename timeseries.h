6566b9001ea9f7294934af800c779915d23c5538
yarintz33 <yarin.tzdaka@live.biu.ac.il>
2021-11-10 08:22:19 -0800

    hi

--- timeseries.h
+++ timeseries.h
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <vector>
 //#include <string>
-
+#include <sstream>
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
@@ -14,26 +14,35 @@ public:
     TimeSeries(const char* CSVfileName){
         string line;
         ifstream myFile (CSVfileName);
-        vector<(pair<(string), (vector<float)>)> columns;
+        vector<pair<string, vector<float>>> columns;
         int flag = 1;
         if (myFile.is_open())
         {
-            while ( getline (myFile,line) )
-            {
-                std::istringstream ss(line);
-                std::string token
-                while(std::getline(ss, token, ',')) {
+            getline (myFile,line);
+            std::istringstream ss(line);
+            std::string token;
+            while (std::getline(ss, token, ',')) {
+                vector<float> v1;
+                columns.push_back(make_pair(token, v1));
+            }
+
+            while (std::getline (myFile,line)){
+                std::if ss(line);
+                std::string token;
                     if (flag == 1) {
-                        vector<float> v1
-                        columns.push_bac(make_pair(token, v1));
 
                     }
-                    else{
-                        for(int i = 0, i < columns.size(), i++){
-                            columns[i].second = token;
-                        }
-                    }
                 }
+
+
+                    /*else{
+                        for(int i = 0; i < columns.size(); i++){
+                            v1.push_back();
+
+                        }
+                        columns[j].second = v1;
+                    }
+                }*/
                 flag = 0;
 
             }
